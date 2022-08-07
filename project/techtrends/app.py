1c1fb09b6550818606ba59bb91c899e7059688a0
NamTe <tintac_tk3@yahoo.com>
2022-08-08 02:18:13 +0700

    Init db and Step 1

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -2,12 +2,18 @@ import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+import logging
+
+# Global variable
+dbConnectionCount = 0
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global dbConnectionCount
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    dbConnectionCount += 1
     return connection
 
 # Function to get a post using its ID
@@ -36,13 +42,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.info("Article not found the id: %s ", post_id)
       return render_template('404.html'), 404
     else:
+      app.logger.info("Article \"%s\" retrieved!", post["title"])
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info("About page retrieved!")
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -60,11 +69,38 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            app.logger.info("Article \"%s\" created!", title)
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+# Healthcheck endpoint
+@app.route('/healthz')
+def healthz():
+    response = app.response_class(
+        response=json.dumps({"result": "OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+# Metrics endpoint
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    postCount = len(connection.execute('SELECT count(*) FROM posts').fetchall())
+    connection.close()
+    response = app.response_class(
+        response=json.dumps({"db_connection_count": dbConnectionCount, "post_count": postCount}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
 # start the application on port 3111
 if __name__ == "__main__":
+   logging.basicConfig(
+       level=logging.DEBUG,
+       format='%(levelname)s:%(name)s: %(asctime)s, %(message)s')
    app.run(host='0.0.0.0', port='3111')
